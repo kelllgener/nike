3d6525f381df6a0ebbcbefdff9a4c1d5e2694c52
Kel Gener <adriangener01@gmail.com>
2024-06-10 17:40:47 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => (
+  <main className="relative flex">
+    <div className="flex w-full justify-center items-center bg-black text-white h-[100px]">
+      <h1 className="text-blue-900">Hello Word</h1>
+    </div>
+  </main>
+);
+
+export default App;
